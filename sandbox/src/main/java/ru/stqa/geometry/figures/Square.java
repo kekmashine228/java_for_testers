17dae0e9d7af50732be1c5bad76f29f252c7ac96
kekmashine228 <danlyk2@gmail.com>
2026-05-11 00:04:24 +0300

    add first test

--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -2,10 +2,15 @@ package ru.stqa.geometry.figures;
 
 public class Square {
     public static void printSquareArea(double side) {
-        System.out.println("ru.stqa.geometry.figures.Square area with side " + side + " = " + squareArea(side));
+        System.out.println("ru.stqa.geometry.figures.Square area with side " + side + " = " + area(side));
     }
 
-    public static double squareArea(double side) {
+    public static double area(double side) {
         return side * side;
+    }
+
+
+    public static double perimeter(double a) {
+        return 4 * a;
     }
 }
